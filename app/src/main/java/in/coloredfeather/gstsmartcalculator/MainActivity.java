095f19f82478f36d2a21561f9d5c15b03467fb28
anandkumarkparmar <anandkumar.k.parmar@gmail.com>
2018-01-01 21:05:52 +0530

    Start activity added

--- app/src/main/java/in/coloredfeather/gstsmartcalculator/MainActivity.java
+++ app/src/main/java/in/coloredfeather/gstsmartcalculator/MainActivity.java
@@ -6,8 +6,6 @@ import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
-import org.w3c.dom.Text;
-
 import java.text.DecimalFormat;
 import java.util.HashMap;
 import java.util.Map;
